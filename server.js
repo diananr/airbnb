0b970bdcaff47473d2a8a2f09d3290650163d93f
diananr <navarroruiz.diana>
2016-12-12 10:16:42 -0500

    add search responsive

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 
 var app = express();
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/dist"));
 
 app.listen(3028 , function(){
   console.log("encendido");
