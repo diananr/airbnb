377e089dfd3a57a306249c97fce1147caa098706
efarfan2393 <e.farfanvaldivia@gmail.com>
2016-12-12 16:09:41 -0500

    firt commit footer

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var app = express();
 
 app.use(express.static(__dirname + "/dist"));
 
+app.use(express.static(__dirname + "/assets"));
+
 app.listen(3028 , function(){
   console.log("encendido");
 });
