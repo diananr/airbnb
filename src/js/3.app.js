12b12df4d11e9a0151dec80ede5d8a008f044447
anbelespinal <anabelespinal>
2016-12-12 19:44:24 -0500

    slider to ipad_768

--- src/js/3.app.js
+++ src/js/3.app.js
@@ -7,17 +7,17 @@ $(document).ready(loadPag);
 
 // anabel: slider
 
-var slideIndex = 0;
-carousel();
+// var slideIndex = 0;
+// carousel();
 
-function carousel() {
-    var i;
-    var x = document.querySelectorAll(".slider-carrusel");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1}
-  $(x[slideIndex-1]).fadeIn(20);
-    setTimeout(carousel, 10000);
-}
+// function carousel() {
+//     var i;
+//     var x = document.querySelectorAll(".slider-carrusel");
+//     for (i = 0; i < x.length; i++) {
+//       x[i].style.display = "none";
+//     }
+//     slideIndex++;
+//     if (slideIndex > x.length) {slideIndex = 1}
+//   $(x[slideIndex-1]).fadeIn(20);
+//     setTimeout(carousel, 10000);
+// }
